30a3177d73eaaf71a1623e965ea0ed8cf984b5bd
daniilprohorov <daniilprohorov@gmail.com>
2021-06-24 11:05:25 +0300

    simple working version

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Lib
 import System.Environment
+import System.Cron
+import Data.Aeson
+import Data.Text (Text, pack)
+import Control.Concurrent.MVar
+import Data.List (foldl')
+import Data.Time
+
+import qualified Data.HashMap.Strict as HM
 
 main :: IO ()
 main = do
+    storage <- initStorage ["London", "Moscow"]
+    state <- getState storage
+    tids <- execSchedule $ do
+        addJob (updateStorage state) "* * * * *"
+    startApp state
+    print tids
+
+getState storage = do
   [Just port, Just apiKey, Just apiRoot, Just apiPath] <- mapM lookupEnv ["PORT", "API_KEY", "API_ROOT", "API_PATH"]
-  let state = State (read port :: Int) apiKey apiRoot apiPath ["London"]
-  startApp state
+  let state = State (read port :: Int) (pack apiKey) (pack apiRoot) (pack apiPath) storage
+  return state
+
+initStorage :: [Text] -> IO (MVar Object)
+initStorage locations = newMVar $ HM.fromList $ zip locations $ repeat Null
+
+updateStorage :: ApplicationState -> IO ()
+updateStorage state@State{storage} = do
+  storage_ <- takeMVar storage
+  let locations = HM.keys storage_
+  objects <- mapM (weatherRequest state) locations
+  let newStorage = HM.fromList $ zip locations (map Object objects)
+  timeDate <- getZonedTime
+  putStrLn $ "Cashe server update " <> show timeDate
+  putMVar storage newStorage
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,10 +3,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
 {-# LANGUAGE NamedFieldPuns  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Lib
     ( startApp
     , app
     , ApplicationState(..)
+    , weatherRequest
     ) where
 
 import Data.Aeson
@@ -14,9 +17,6 @@ import Data.Aeson.TH
 import Network.Wai hiding (Response)
 import Network.Wai.Handler.Warp
 import Servant
--- import Network.HTTP.Client (newManager, defaultManagerSettings)
--- import Servant.API
---import Servant.Client
 import qualified Data.HashMap.Strict as HM
 import Data.Aeson
 import Data.Proxy
@@ -26,31 +26,22 @@ import Control.Monad.IO.Class
 
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Data.Either (either)
-import Data.Text (pack)
+import Data.Text (pack, unpack)
+import Data.Text (Text(..))
 
 import Control.Concurrent.MVar
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
-
---data Body = Body {responseBody :: String} deriving (Show, Generic)
-
-$(deriveJSON defaultOptions ''User)
-
 apiPort = 80
-
-type API = "weather" :> Get '[JSON] Object
-type APIClient = "weather" :> QueryParam "q" String :> QueryParam "appid" String :> Get '[JSON] (Object)
+-- логично добавить appid в параметры запроса, тогда не надо будет ничего сохранять на уровне кеш сервера
+type API = "weather" :> QueryParam "q" Text :> Get '[JSON] Object
+type APIClient = "weather" :> QueryParam "q" Text :> QueryParam "appid" Text :> Get '[JSON] (Object)
 
 data ApplicationState = State
   { port :: Int
-  , apiKey :: String
-  , apiRoot :: String
-  , apiPath :: String
-  , locations :: [String]
+  , apiKey :: Text
+  , apiRoot :: Text
+  , apiPath :: Text
+  , storage :: MVar Object
   }
 
 
@@ -66,23 +57,30 @@ api = Proxy
 apiclient :: Proxy APIClient
 apiclient = Proxy
 
+controller :: ApplicationState -> Maybe Text -> Handler Object
+controller state@State{storage} (Just location) = do
+  storage_ <- liftIO $ readMVar storage
+  case storage_ HM.!? location of
+      Just (Object value) -> do
+        liftIO $ print $ "Read from cashe. Location = " <> location
+        return value
+      _ -> do
+        liftIO $ print $ "Make request. Location = " <> location
+        res <- liftIO $ weatherRequest state location
+        return res
+
+controller _ Nothing = throwError $ err404 { errBody = "Please enter q=LOCATION query param" }
+
 server :: ApplicationState -> Server API
-server state = do
-  res <- liftIO $ weatherRequest state
-  let fromEither = either (\x -> HM.singleton (pack "error") (String $ pack $ show x)) id
+server state = controller state
+
+weatherRequest :: ApplicationState -> Text -> IO (Object)
+weatherRequest state@State{apiKey, apiRoot, apiPath} location = do
+  -- тут сделано по колхозному!!
+  let fromEither = either (\x -> HM.singleton (pack "Error") (String $ pack $ show x)) id
+  manager <- newManager defaultManagerSettings
+  res <- runClientM (getWeather (Just location) (Just apiKey)) (mkClientEnv manager (BaseUrl Http (unpack apiRoot) 80 (unpack apiPath)))
   return $ fromEither res
 
-weatherRequest state@State{apiKey, apiRoot, apiPath, locations} = do
-  let location = head locations
-  manager <- newManager defaultManagerSettings
-  res <- runClientM (getWeather (Just location) (Just apiKey)) (mkClientEnv manager (BaseUrl Http apiRoot 80 apiPath))
-  return res
-
-getWeather :: Maybe String -> Maybe String -> ClientM (Object)
+getWeather :: Maybe Text -> Maybe Text -> ClientM (Object)
 getWeather = client apiclient
-
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
